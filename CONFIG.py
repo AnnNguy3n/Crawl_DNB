fef99d629493b9e6f6277cd0a2575f6966a6e2ec
AnnNguy3n <annguyen@vis-laboratory.com>
2023-09-18 13:59:14 +0700

    .

---
+++ CONFIG.py
@@ -0,0 +1,4 @@
+PROXY_FILE_PATH = r"C:\Users\AnnNg\OneDrive\Desktop\Crawl_DNB\Webshare 250 proxies.txt"
+FOLDER_EXTENSION = r"C:\Users\AnnNg\OneDrive\Desktop\Crawl_DNB\Proxies"
+FOLDER_DATA = r"H:\My Drive\Data_DNB"
+MAIN_PAGE = "https://www.dnb.com/business-directory.html#CompanyProfilesPageNumber=1&ContactProfilesPageNumber=1&DAndBMarketplacePageNumber=1&IndustryPageNumber=1&SiteContentPageNumber=1&tab=Company%20Profiles"

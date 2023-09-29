d027b1d1e6e1c43223aaae4861c936db535ac220
AnnNguy3n <nguyenhuuan.k64.hust@gmail.com>
2023-09-29 20:48:24 +0700

    .

--- crawler.py
+++ crawler.py
@@ -440,7 +440,7 @@ class Crawler:
             title = br.driver.title
             if "Company Profile" in title and "Dun & Bradstreet" in title:
                 status = 1
-            elif "Access Denied" in title:
+            elif "Access Denied" in title or "Oh no! 500 Error" in title:
                 status = 2
             else:
                 status = 3
